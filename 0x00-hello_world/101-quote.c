48e24f03c2c58acb46fa2ca125af7b53cb72821f
Charles Misheal <charlesmisheal@gmail.com>
2022-06-30 05:48:21 -0700

    rdm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* Description: main - Prints out a string to stdout.
-* Return: 0 if success.
+* main - Prints exactly "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error
+* Return:Always 0 (success)
 */
-int main(void) 
+int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
