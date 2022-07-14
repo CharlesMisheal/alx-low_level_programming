92edb87602975c61e164b00ae72cd7010b17114a
Charles Misheal <charlesmisheal@gmail.com>
2022-07-14 15:50:28 -0700

    taask12

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,10 +9,10 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write your line of code here...
+* write your line of code here..
 * Remember:
 * - you are not allowed to use a
-* - you are not allowed to modify p
+* - you are not allowed to modify
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
