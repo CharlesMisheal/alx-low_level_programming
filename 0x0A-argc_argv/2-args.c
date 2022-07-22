52e0eabd6aa385f0eafb733425ded710812127fa
Charles Misheal <charlesmisheal@gmail.com>
2022-07-22 16:09:03 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+nclude "main.h"
+
+/**
+* main - the entry point
+* @argc: argument counter
+* @argv: array of pointer to strings
+* Created by Charles Misheal
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+
+{
+
+int i;
+i = 0;
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
