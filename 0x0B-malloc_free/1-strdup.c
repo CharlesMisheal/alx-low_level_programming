eac5e4fdf738b17e64b8b4aca51c2dbb27770e73
Charles Misheal <charlesmisheal@gmail.com>
2022-07-25 15:31:33 -0700

    tsk1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,17 +12,22 @@
 char *_strdup(char *str)
 
 {
-
-char *strout;
 unsigned int i, j;
+
+char *s;
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-
-strout = (char *)malloc(sizeof(char) * (i + 1));
-if (strout == NULL)
+for (i = 0; str[i]; i++)
+;
+i++;
+s = malloc(i * sizeof(char));
+if (s == NULL)
+{
 return (NULL);
-for (j = 0; j <= i; j++)
-strout[j] = str[j];
-return (strout);
+}
+for (j = 0; j < i; j++)
+{
+s[j] = str[j];
+}
+return (s);
 }
