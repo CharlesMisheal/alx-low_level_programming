24efa172973e8bdb66f5d634159e61f05709dabb
Charles Misheal <charlesmisheal@gmail.com>
2022-07-25 15:18:37 -0700

    tsk1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,11 +18,11 @@ unsigned int i, j;
 if (str == NULL)
 return (NULL);
 for (i = 0; str[i] != '\0'; i++)
-;
+
 strout = (char *)malloc(sizeof(char) * (i + 1));
 if (strout == NULL)
 return (NULL);
 for (j = 0; j <= i; j++)
-strout[j] = str[j]
+strout[j] = str[j];
 return (strout);
 }
