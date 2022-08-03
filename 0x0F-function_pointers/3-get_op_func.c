27404a818db0d6a3bb78c056dfdbea5a04807995
Charles Misheal <charlesmisheal@gmail.com>
2022-08-03 14:01:33 -0700

    tsk3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
 
+#include <stdlib.h>
+
+#include <stdio.h>
+
 /**
-* get_op_func - selects the correct function to perform the operation
-* asked by the user.
-* @s: char operator
-* Created by Charles Misheal
-* Return: pointer
+* get_op_func - select the correct operation function asked by user
+* @s: operator argument
+* Return: function pointer corresponding to operator given
 */
 
 int (*get_op_func(char *s))(int, int)
@@ -13,21 +15,21 @@ int (*get_op_func(char *s))(int, int)
 {
 
 op_t ops[] = {
-
 {"+", op_add},
 {"-", op_sub},
-
 {"*", op_mul},
 {"/", op_div},
 {"%", op_mod},
 {NULL, NULL}
 };
-int i = 0;
-while (i < 10)
+
+int i;
+i = 0;
+while (i < 5)
 {
-if (s[0] == ops->op[i])
-break;
+if (ops[i].op[0] == s[0])
+return (ops[i].f);
 i++;
 }
-return (ops[i / 2].f);
+return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,34 +1,71 @@
-#include "3-calc.h"
+#include <stdio.h>
 
 #include <stdlib.h>
 
-#include <string.h>
-
-
 /**
-* get_op_func - ...
-* @s: ...
-* Created by Charles Misheal
-* Return: ...
+* op_add - addition operator
+* @a: first int
+* @b: second int
+* Return: sum of 2 numbers
 */
 
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - subtraction operator
+* @a: first in
+* @b: second int
+* Return: difference of 2 numbers
+*/
+
+int op_sub(int a, int b)
 
 {
-op_t ops[] = {
-{ "+", op_add },
-{ "-", op_sub },
-{ "*", op_mul },
-{ "/", op_div },
-{ "%", op_mod },
-{ NULL, NULL }
-};
-int i = 0;
-while (i < 5)
-{
-if (strcmp(s, ops[i].op) == 0)
-return (ops[i].f);
-i++;
+return (a - b);
 }
-return (0);
+
+/**
+* op_mul - product of 2 numbers
+* @a: first int
+* @b: second int
+* Return: product of the numbers
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - division of 2 numbers
+* @a: first int
+* @b: second int
+* Return: result of the division of the numbers
+*/
+
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+
+/**
+* op_mod - get remainder of the division of 2 numbers
+* @a: first int
+* @b: second int
+* Return: remainder of division of the numbers
+*/
+
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
