24efffadab3f880e0f94eb627f2d0ed5b3f55b3c
Charles Misheal <charlesmisheal@gmail.com>
2022-08-11 02:08:44 -0700

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+/**
+* bbmain - function executted before main
+* Created by Charles Misheal
+* Return: void
+*/
+
+void __attribute__ ((constructor)) bbmain()
+
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+
+	printf("I bore my house upon my back!\n");
+
+}
