72603fea6113c7529a4ce7857e889b7690bd1bf6
Charles Misheal <charlesmisheal@gmail.com>
2022-08-15 02:39:03 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+
+/**
+* print_listint - prints all the elements of a listint_t list
+* @h: head of list
+* Created by Charles Misheal
+* Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+
+{
+
+	size_t num_nodes = 0;
+
+
+
+	while (h != NULL)
+
+	{
+
+		printf("%d\n", h->n);
+
+		h = h->next;
+
+		num_nodes++;
+
+	}
+
+	return (num_nodes);
+
+}
+
+Footer
