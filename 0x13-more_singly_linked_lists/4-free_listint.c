1b98c4840c047f9b033e69047c81692b1180f8a8
Charles Misheal <charlesmisheal@gmail.com>
2022-08-15 03:00:20 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+
+/**
+* free_listint - frees a listint_t list
+* @head: head of list
+* Created by Charles Misheal
+* Return: void
+*/
+
+void free_listint(listint_t *head)
+
+{
+
+	listint_t *temp;
+
+
+
+	while ((temp = head) != NULL)
+
+	{
+
+		head = head->next;
+
+		free(temp);
+
+	}
+
+}
