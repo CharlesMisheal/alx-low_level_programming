c7861e145752c3c2c72d97e7eaaed21fb92afdc1
Charles Misheal <charlesmisheal@gmail.com>
2022-08-16 05:10:57 -0700

    file update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,12 @@
-#ifndef LISTS_H
+#ifndef _LISTS_
 
-#define LISTS_H
+#define _LISTS_
 
 
-
-#include <stdlib.h>
-
-#include <string.h>
 
 #include <stdio.h>
 
+#include <stdlib.h>
 
 
 /**
@@ -31,6 +28,23 @@ typedef struct listint_s
 } listint_t;
 
 
+
+/**
+* struct listp_s - singly linked list
+* @p: pointers of nodes
+* @next: points to the next node
+* Description: singly linked list of pointers
+*/
+
+typedef struct listp_s
+
+{
+
+	void *p;
+
+	struct listp_s *next;
+
+} listp_t;
 
 
 
@@ -63,8 +77,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 listint_t *find_listint_loop(listint_t *head);
-
-
 
 
 
