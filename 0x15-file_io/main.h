c7f8962af6bb882f004b9f4e3d4057cc9bd852cc
Charles Misheal <charlesmisheal@gmail.com>
2022-08-22 13:54:37 -0700

    file updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,17 +4,17 @@
 
 
 
+#include <unistd.h>
+
 #include <sys/types.h>
 
 #include <sys/stat.h>
 
 #include <fcntl.h>
 
-#include <unistd.h>
-
-#include <stdlib.h>
 
 
+int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
@@ -24,4 +24,4 @@ int append_text_to_file(const char *filename, char *text_content);
 
 
 
-#endif
+#endif /*MAIN_H*/
